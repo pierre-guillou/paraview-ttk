c9ad1b198390c3b679746a692cd0bee2ae68aaec
Pierre Guillou <pierre.guillou@lip6.fr>
2019-11-27 16:34:32 +0100

    [DEB] Add function to install build dependencies

--- create_deb.sh
+++ create_deb.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+function install_build_deb {
+    sudo apt install -y \
+         ccache \
+         cmake-curses-gui \
+         g++ \
+         git \
+         libegl1-mesa-dev \
+         libpython3-dev \
+         libqt5x11extras5-dev \
+         libxt-dev \
+         openssh-server \
+         qt5-default \
+         qttools5-dev \
+         qtxmlpatterns5-dev-tools \
+         vim
+}
+
 function create_deb {
     local builddir='build_deb'
     rm -rf $builddir
